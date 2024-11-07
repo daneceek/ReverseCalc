9d1c0d08f8cdd45ecdaa53a1057aab6bdc842495
Danny Kročil <dannykrocil@seznam.cz>
2024-11-07 21:37:08 +0100

    update

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
-#!/usr/bin/env python3
+
 
 from os.path import basename, splitext
 import tkinter as tk
 from tkinter import font
 from operation import operation1, operation2
-# from tkinter import ttk
+import operator
+import math
 
 
 class MyEntry(tk.Entry):
     def __init__(self, master=None, cnf={}, **kw):
         super().__init__(master, cnf, **kw)
 
-        if not "textvariable" in kw:
+        if "textvariable" not in kw:
             self.variable = tk.StringVar()
             self.config(textvariable=self.variable)
         else:
@@ -33,7 +34,7 @@ class MyListbox(tk.Listbox):
             self.delete("end")
             return x
         else:
-            raise IndexError
+            raise IndexError("The stack is empty")
 
 
 class Application(tk.Tk):
@@ -42,138 +43,104 @@ class Application(tk.Tk):
 
     def __init__(self):
         super().__init__(className=self.name)
-
         self.option_add("*Font", self.font_settings)
 
         self.title(self.name)
         self.bind("<Escape>", self.destroy)
 
+        
         self.lbl = tk.Label(self, text="Reverzní kalkulátor")
         self.listbox = MyListbox(master=self)
-        self.btn = tk.Button(self, text="Destroy", command=self.destroy)
+        self.btn = tk.Button(self, text="Opustit", command=self.destroy)
         self.entry = MyEntry(master=self)
         self.entry.bind("<Return>", self.enterHandler)
         self.entry.bind("<KP_Enter>", self.enterHandler)
+        
+       
+        self.status_bar = tk.Label(self, text="", fg="red")
+        
+        
+        self.history = []
+        self.history_index = -1
+        self.entry.bind("<Up>", self.showPreviousHistory)
+        self.entry.bind("<Down>", self.showNextHistory)
 
+        
         self.lbl.pack()
         self.listbox.pack()
         self.entry.pack()
         self.btn.pack()
+        self.status_bar.pack(fill="x")
         self.entry.focus()
 
     def enterHandler(self, event):
-        for token in self.entry.value.split():
+        input_text = self.entry.value.strip()
+        self.history.append(input_text)
+        self.history_index = len(self.history)
+        
+        for token in input_text.split():
             try:
                 self.listbox.insert("end", float(token))
+                self.updateStatus("")  
             except ValueError:
                 self.tokenProcess(token)
-        self.entry.value = ""
-        self.listbox.see("end")
-#!/usr/bin/env python3
-
-from os.path import basename, splitext
-import tkinter as tk
-from tkinter import font
-from operation import operation1, operation2
-# from tkinter import ttk
-
-
-class MyEntry(tk.Entry):
-    def __init__(self, master=None, cnf={}, **kw):
-        super().__init__(master, cnf, **kw)
-
-        if not "textvariable" in kw:
-            self.variable = tk.StringVar()
-            self.config(textvariable=self.variable)
-        else:
-            self.variable = kw["textvariable"]
-
-    @property
-    def value(self):
-        return self.variable.get()
-
-    @value.setter
-    def value(self, new: str):
-        self.variable.set(new)
-
-
-class MyListbox(tk.Listbox):
-    def pop(self):
-        if self.size() > 0:
-            x = self.get("end")
-            self.delete("end")
-            return x
-        else:
-            raise IndexError
-
-
-class Application(tk.Tk):
-    name = "RevCal"
-    font_settings = ("Verdana", 16)
-
-    def __init__(self):
-        super().__init__(className=self.name)
-
-        self.option_add("*Font", self.font_settings)
-
-        self.title(self.name)
-        self.bind("<Escape>", self.destroy)
-
-        self.lbl = tk.Label(self, text="Reverzní kalkulátor")
-        self.listbox = MyListbox(master=self)
-        self.btn = tk.Button(self, text="Destroy", command=self.destroy)
-        self.entry = MyEntry(master=self)
-        self.entry.bind("<Return>", self.enterHandler)
-        self.entry.bind("<KP_Enter>", self.enterHandler)
-
-        self.lbl.pack()
-        self.listbox.pack()
-        self.entry.pack()
-        self.btn.pack()
-        self.entry.focus()
-
-    def enterHandler(self, event):
-        for token in self.entry.value.split():
-            try:
-                self.listbox.insert("end", float(token))
-            except ValueError:
-                self.tokenProcess(token)
+                
         self.entry.value = ""
         self.listbox.see("end")
 
     def tokenProcess(self, token):
-        if token in operation2:
-            b = self.listbox.get("end")
-            self.listbox.delete("end")
-            a = self.listbox.get("end")
-            self.listbox.delete("end")
-            r = operation2[token](a,b)
-            self.listbox.insert("end", r)
-
-        if token in operation1:
-            a = self.listbox.get("end")
-            self.listbox.delete("end")
-            r = operation1[token](a)
-            self.listbox.insert("end", r)
-            
-        match token:
-            case "sw" if self.listbox.size() >= 2:
-                    b = self.listbox.get("end")
-                    self.listbox.delete("end")
-                    a = self.listbox.get("end")
-                    self.listbox.delete("end")
-                    self.listbox.insert("end", b)
+        try:
+            if token in operation2:
+                if self.listbox.size() < 2:
+                    raise ValueError("Nedostatek čísel")
+                b = float(self.listbox.pop())
+                a = float(self.listbox.pop())
+                if token == '/' and b == 0:
                     self.listbox.insert("end", a)
-            case "del" if self.listbox.size() >= 1:
-                    self.listbox.delete("end")
+                    raise ZeroDivisionError("Dělení nulou")
+            
+                result = operation2[token](a, b)
+                self.listbox.insert("end", result)
+                self.updateStatus("")  
+
+            elif token in operation1:
+                if self.listbox.size() < 1:
+                    raise ValueError("Nedostatek čísel")
+                a = float(self.listbox.pop())
+                result = operation1[token](a)
+                self.listbox.insert("end", result)
+                self.updateStatus("") 
+
+            
+
+            elif token == "del" and self.listbox.size() >= 1:
+                self.listbox.delete("end")
+            else:
+                raise ValueError(f"Neznámý token: {token}")
+        
+        except (ValueError, ZeroDivisionError) as e:
+            self.updateStatus(str(e))
+
+    def updateStatus(self, message):
+        self.status_bar.config(text=message)
+
+    def showPreviousHistory(self, event):
+        if self.history and self.history_index > 0:
+            self.history_index -= 1
+            self.entry.value = self.history[self.history_index]
+
+    def showNextHistory(self, event):
+        if self.history and self.history_index < len(self.history) - 1:
+            self.history_index += 1
+            self.entry.value = self.history[self.history_index]
+        elif self.history_index == len(self.history) - 1:
+            self.history_index += 1
+            self.entry.value = ""  
 
     def destroy(self, event=None):
         super().destroy()
 
 
+
 app = Application()
 app.mainloop()
-
-import operator
-import math
-
